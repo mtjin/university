c38b696bf2f8b419b2f4fe8bd780d8c21521b389
seungeon <tmddjs210@naver.com>
2019-10-19 01:02:54 +0900

    과제설명 추가 및 4주차과제 업로드

---
+++ compiler/04/Test.java
@@ -0,0 +1,18 @@
+import java.io.PrintStream;
+
+public class Test {
+	
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+    public static int add(int n, int n2) {
+        int n3 = n + n2;
+        return n3;
+    }
+
+    public static void main(String[] arrstring) {
+        int n = 33;
+        System.out.println(Test.add(1, n));
+    }
+}
